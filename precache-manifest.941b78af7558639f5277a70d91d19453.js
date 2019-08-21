9bfce0a32c0c4616aa7e23d8e3a2a931e2610cd1
kevinwit <kevin.zou@honeywell.com>
2019-08-21 16:02:31 +0800

    Updates

---
+++ precache-manifest.941b78af7558639f5277a70d91d19453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84f2dd8db83c06e4c1c44943b4d06f8",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "03c198450363462425b4",
+    "url": "/dashboard/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "3490d7892b739f3990c9",
+    "url": "/dashboard/static/css/main.879281ec.chunk.css"
+  },
+  {
+    "revision": "03c198450363462425b4",
+    "url": "/dashboard/static/js/2.43cbe07f.chunk.js"
+  },
+  {
+    "revision": "3490d7892b739f3990c9",
+    "url": "/dashboard/static/js/main.2fe613f2.chunk.js"
+  },
+  {
+    "revision": "81def2a9b0ec61a2985f",
+    "url": "/dashboard/static/js/runtime~main.0d6b92b7.js"
+  }
+]);
